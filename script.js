fb86bcdad69d5e6a9b8cfab6fc5b01235d832d90
4wan-cloud <setiawan.alif.sukamto@gmail.com>
2025-07-06 12:24:03 +0700

    Update script.js
    tambahan fungsi opentab

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ function openTab(tabName) {
       tab.classList.add('hidden');
     });
     document.querySelectorAll('.tab-button').forEach(button => {
-      button.classList.remove('bg-blue-500', 'text-white');
-      button.classList.add('bg-gray-300', 'text-gray-800');
+      button.classList.remove('text-blue-500', 'border-blue-500');
+      button.classList.add('text-gray-600', 'border-transparent');
     });
     document.getElementById(tabName).classList.remove('hidden');
-    event.currentTarget.classList.remove('bg-gray-300', 'text-gray-800');
-    event.currentTarget.classList.add('bg-blue-500', 'text-white');
+    event.currentTarget.classList.remove('text-gray-600', 'border-transparent');
+    event.currentTarget.classList.add('text-blue-500', 'border-blue-500');
     // Kirim event ke Google Analytics
     gtag('event', 'tab_click', {
       'event_category': 'Tab Navigation',
